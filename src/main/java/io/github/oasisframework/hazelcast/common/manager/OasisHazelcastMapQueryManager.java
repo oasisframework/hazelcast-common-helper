754053e9b893febdf9efe77424ea46cf29baff6c
muhsincaliskan <muhsincaliskan96@gmail.com>
2024-12-02 15:19:58 +0300

    added hazelcast set value capabilities

--- src/main/java/io/github/oasisframework/hazelcast/common/manager/OasisHazelcastMapQueryManager.java
+++ src/main/java/io/github/oasisframework/hazelcast/common/manager/OasisHazelcastMapQueryManager.java
@@ -1,5 +1,9 @@
 package io.github.oasisframework.hazelcast.common.manager;
 
+import java.util.concurrent.TimeUnit;
+
 public interface OasisHazelcastMapQueryManager {
 	<K,V> void addValueToMap(String mapName, K key, V value);
+	<K, V> void addValueToMap(String mapName, K key, V value, long ttl, TimeUnit ttlTimeUnit);
+	<K, V> void addValueToMap(String mapName, K key, V value, long ttl, TimeUnit ttlTimeUnit, long maxIdle, TimeUnit maxIdleTimeUnit);
 }
